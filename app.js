20165811473a0eab3947168d179c39ed9883926d
osefatul <sefaullahomar2@gmail.com>
2021-09-15 22:15:48 -0700

    starting the express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.send("Home page");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
